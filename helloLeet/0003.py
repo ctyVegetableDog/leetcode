1287a6c93a0cc3d01fd64892fc2f4bed842a17a1
ctyVegetableDog <821789905@qq.com>
2019-10-20 15:14:25 +0100

    dp

---
+++ helloLeet/0003.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = len(s)
+        st = set()
+        i = 0
+        j = 0
+        ans = 0
+        while(i < l and j < l):
+            if s[j] not in st:
+                st.add(s[j])
+                j += 1
+                ans = max(ans, j - i)
+            else:
+                st.remove(s[i])
+                i += 1
+        return ans
