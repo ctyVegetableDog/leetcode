464bf2b1543ff59301a7803295bd216aa2fcbfb7
ctyVegetableDog <821789905@qq.com>
2019-11-04 15:37:39 +0000

    recurion (good ques)

---
+++ helloLeet/0022.cpp
@@ -0,0 +1,15 @@
+//recurion good ques
+
+class Solution {
+public:
+    vector<string> res;
+    void addStrings(string s,int n, int m, int tar) {
+        if (s.length() == 2 * tar) res.push_back(s);
+        if (n < tar) addStrings(s + '(' , n + 1, m,tar);
+        if (m < n) addStrings(s + ')', n, m + 1, tar);
+    }
+    vector<string> generateParenthesis(int n) {
+        addStrings("", 0, 0, n);
+        return res;
+    }
+};
