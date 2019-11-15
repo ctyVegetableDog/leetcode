0d41048a3a3c3ecbf4f2968f064b57292bd50a99
ctyVegetableDog <821789905@qq.com>
2019-11-15 10:22:57 +0000

    bit oper

---
+++ helloLeet/0136.cpp
@@ -0,0 +1,10 @@
+//bit oper (good ques)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for (int n : nums) res^=n;
+        return res;
+    }
+};
