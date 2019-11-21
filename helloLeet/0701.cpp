0cdd908b021c9c4c09a2ed8f48d3bd374a24bdc7
ctyVegetableDog <821789905@qq.com>
2019-11-21 15:48:14 +0000

    insert a new node into BST

---
+++ helloLeet/0701.cpp
@@ -0,0 +1,23 @@
+//Insert a new node into BST
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) {
+            TreeNode* nd = new TreeNode(val);
+            return nd;
+        }
+        if (root->val > val) root->left = insertIntoBST(root->left, val);
+        else root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
