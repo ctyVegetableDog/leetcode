efe7290ab0e99681911c25538b31de4c1cefbb0b
ctyVegetableDog <821789905@qq.com>
2019-11-30 16:30:27 +0000

    complete backpack

---
+++ helloLeet/0322.cpp
@@ -0,0 +1,15 @@
+//complete backpack
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);//backpack's capacity is amount, find the items which can fill up the back without overflow
+        dp[0] = 0;//we don't need any item if the backpack's capacity is 0
+        for (int i = 1; i <= amount ; i++) {
+            for (int j = 0; j < coins.size(); j ++) {
+                if (coins[j] <= i) dp[i] = min(dp[i], dp[i - coins[j]] + 1);//choose coins[j] or not
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+};
