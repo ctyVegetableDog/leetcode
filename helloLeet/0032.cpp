8e9d72880ae027dd7c96cd5474280fe7290a6746
ctyVegetableDog <821789905@qq.com>
2019-12-14 14:13:16 +0000

    longest valid parentheses (stack or dp)

---
+++ helloLeet/0032.cpp
@@ -0,0 +1,19 @@
+//longest valid parentheses (stack, (dp could be worked as well))
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int res = 0;
+        stack<int> st;
+        st.push(-1);
+        for (int i = 0; i < s.size(); i ++) {
+            if (s[i] == '(') st.push(i);
+            else {
+                st.pop();
+                if (st.empty()) st.push(i);
+                else res = max(res, i - st.top());
+            }
+        }
+        return res;
+    }
+};
