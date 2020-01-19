1e0274a9e46b723653db7dc7b3b5bf7ff0bcf7bc
ctyVegetableDog <821789905@qq.com>
2020-01-19 17:55:39 +0000

    reservoir sampling (interesting ques)

---
+++ helloLeet/0382.cpp
@@ -0,0 +1,40 @@
+//reservoir samnpling (interesting question)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode *h;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        h = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int n = 1;
+        int res = -1;
+        ListNode *p = h;
+        while (p) {
+            int r = rand() % n + 1;
+            if (r == 1) res = p->val;
+            p = p->next;
+            n++;
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
