7498ac4b792a09508659f5729ec082a767eb7c91
ctyVegetableDog <821789905@qq.com>
2020-02-06 12:38:06 +0000

    remove duplicates from sorted array 2 (logic)

---
+++ helloLeet/0080.cpp
@@ -0,0 +1,11 @@
+//Remove duplicates from sorted array 2 (logic)
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int n : nums) 
+            if (i < 2 || n > nums[i - 2]) nums[i++] = n;
+        return i;
+    }
+};
