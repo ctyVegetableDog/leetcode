950f7b3df6ffe5559762eedb045082b67ca63c10
ctyVegetableDog <821789905@qq.com>
2020-02-10 11:18:58 +0000

    Best Time to Buy and Sell Stock II

---
+++ helloLeet/0122.cpp
@@ -0,0 +1,17 @@
+//Best Time to Buy and Sell Stock II
+
+/*
+	c > b > a
+	must have (c - )b + (b -a) == c - a
+*/
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res = 0;
+        for (int i = 1; i < prices.size(); i ++) {
+            if (prices[i] > prices[i - 1]) res += prices[i] - prices[i - 1];
+        }
+        return res;
+    }
+};
