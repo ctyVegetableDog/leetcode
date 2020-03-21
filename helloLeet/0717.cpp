aa9b19e6f3c9891d3afbdcf01da4674ff04140c8
ctyVegetableDog <821789905@qq.com>
2020-03-21 21:14:25 +0000

    1-bit and 2-bit Characters

---
+++ helloLeet/0717.cpp
@@ -0,0 +1,10 @@
+//1-bit and 2-bitt Characters
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        int cnt = 0;
+        for (int i = bits.size() - 2; i >= 0 && bits[i] == 1; --i) cnt ++;
+        return !(cnt % 2);
+    }
+};
