5e5feffb2c06cb46b65b6883c062d9ae6674aa00
ctyVegetableDog <821789905@qq.com>
2020-05-21 21:24:36 +0100

    Two Sum 2  - Input array iis sorted

---
+++ helloLeet/0167.cpp
@@ -0,0 +1,14 @@
+//Two Sum 2 - Input array is sorted
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] == target) return {l + 1, r + 1};
+            else if (numbers[l] + numbers[r] > target) --r;
+            else ++l;
+        }
+        return {0, 0};
+    }
+};
