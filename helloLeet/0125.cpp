5445cbc9f21045c950a1c73af05b157a5ad273bd
ctyVegetableDog <821789905@qq.com>
2021-02-20 16:26:14 +0800

    check palindrome

---
+++ helloLeet/0125.cpp
@@ -0,0 +1,17 @@
+// check palindrome
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            while (l < s.size() && !isalnum(s[l])) ++l;
+            while (r >= 0 && !isalnum(s[r])) --r;
+            if (l < r) {
+                if (tolower(s[l]) != tolower(s[r])) return false;
+                ++l;
+                --r;
+            }
+        }
+        return true;
+    }
+};
