aeda629354e3a4b6719275a462a7522e7f56c7a6
ctyVegetableDog <821789905@qq.com>
2021-04-24 16:21:40 +0800

    combinationSum4(dp)

---
+++ helloLeet/0377.cpp
@@ -0,0 +1,14 @@
+// combinationSum4 (dp)
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= target; ++i) {
+            for (int &num : nums) {
+                if (i >= num && dp[i] < INT_MAX - dp[i - num]) dp[i] += dp[i - num];
+            }
+        }
+        return dp[target];
+    }
+};
